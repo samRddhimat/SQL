02ad1e307db3bb43e074d7bc5e332623b6111ed0
samRddhimat <seenivasan.palaniappan@outlook.com>
2026-09-26 01:23:33 +0800

    pyspark version added flight data analysis qtexa

--- flightdataanalysis.sql
+++ flightdataanalysis.sql
@@ -3,3 +3,19 @@ select count(flightid), trip_month from (select distinct flightid, month(cast(jd
 or 
 select count(flightid), trip_month from (select distinct flightid, month(cast(jdate as date)) trip_month from iceberg.fda.flightdata group by flightid, month(cast(jdate as date)) ) x group by trip_month order by 2;
 
+
+
+%spark.pyspark
+
+from pyspark.sql.functions import sum, count, month, to_date, countDistinct
+# from pyspark.sql.column import cast
+
+dffda = spark.table("iceberg.fda.flightdata")
+
+dffda \
+.withColumn("jdate", to_date("jdate","yyyy-MM-dd")) \
+.groupBy(month("jdate").alias("trip_month")) \
+.agg(countDistinct("flightid").alias("total_flights")) \
+.orderBy("trip_month") \
+.show()
+
